9940ab0419ecb2ee26872fe081f6b242db469af1
Diegiwg <diegiwg@gmail.com>
2024-04-26 22:32:49 -0300

    feat: push allowed to work with String

--- vm/nim/main.nim
+++ vm/nim/main.nim
@@ -7,8 +7,8 @@ type
         VK_Void, VK_String, VK_Int
 
     Value = object
-        case kind: ValueKind  
-        of VK_Void: v: void
+        case kind: ValueKind
+        of VK_Void: discard
         of VK_String: s: string
         of VK_Int: i: int
 
@@ -53,19 +53,41 @@ proc compile(m: Machine, source: string) =
         let parts = line.splitWhitespace()
         if parts.len == 0: continue
 
-        case parts[0]:
+        let cmd = parts[0]
+        case cmd:
+        
         of "label":
             m.program.add(Instruction(loc: lineCount, kind: Label, ip: instructionCount))
+
         of "pop":
             m.program.add(Instruction(loc: lineCount, kind: Pop))
+
         of "push":
-            m.program.add(Instruction(loc: lineCount, kind: Push, operand: Value(kind: VK_Int, i: parseInt(parts[1]))))
+            if parts.len < 2 or parts[1].len == 0: m.log(lineCount, Log_Warning, "Missing operand for `push` instruction")
+
+            var value: Value = Value(kind: VK_Void)
+
+            if value.kind == VK_Void:
+                try: value = Value(kind: VK_Int, i: parseInt(parts[1]))
+                except: discard
+
+            let rest: string = join(parts[1..^1], " ")
+            if value.kind == VK_Void:
+                if rest[0] == '"' and rest[^1] == '"':
+                    value = Value(kind: VK_String, s: rest[1..^2])
+
+            if value.kind == VK_Void: m.log(lineCount, Log_Warning, fmt("Unknown operand `{rest}` for `push` instruction. `{VK_Void}` assumed"))
+
+            m.program.add(Instruction(loc: lineCount, kind: Push, operand: value))
+
         of "add":
             m.program.add(Instruction(loc: lineCount, kind: Add))
+
         of "print":
             m.program.add(Instruction(loc: lineCount, kind: Print))
+
         else:
-            m.log(lineCount, Log_Warning, fmt("Unknown instruction '{parts[0]}' skipped"))
+            m.log(lineCount, Log_Warning, fmt("Unknown instruction `{cmd}` skipped"))
             continue
 
         inc instructionCount
@@ -93,11 +115,17 @@ proc run(m: Machine) =
             m.stack.add(instruction.operand)
 
         of Add:
-            if m.stack.len < 2: m.log(instruction.loc, Log_Error, "Not enough values on stack for + operation")
-            let a = m.stack.pop
+            if m.stack.len < 2: m.log(instruction.loc, Log_Error, "Not enough values on stack for `add` operation")
+
             let b = m.stack.pop
-            if a.kind != b.kind: m.log(instruction.loc, Log_Error, "Incompatible types for + operation")
-            m.stack.add(Value(kind: VK_Int, i: a.i + b.i))
+            let a = m.stack.pop
+
+            if a.kind != b.kind: m.log(instruction.loc, Log_Error, fmt"Incompatible types for `add` operation. Try to add `{b.kind}` to `{a.kind}`")
+            
+            case a.kind:
+            of VK_Int: m.stack.add(Value(kind: VK_Int, i: a.i + b.i))
+            of VK_String: m.stack.add(Value(kind: VK_String, s: a.s & b.s))
+            of VK_Void: discard
 
         inc m.ip
 
@@ -105,20 +133,26 @@ when isMainModule:
     let argv = commandLineParams()
 
     if len(argv) == 0:
-        echo "Missing file path"
-        quit(1)
+        echo "Usage: plang <file.pasm>"
+        quit 1
     
-    if not fileExists(argv[0]):
-        echo "File not found: ", argv[0]
-        quit(1)
+    let sourcePath = argv[0]
+    
+    if not fileExists(sourcePath):
+        echo fmt"File not found: `{sourcePath}`"
+        quit 1
 
-    let source: string = readFile(argv[0])
+    if not sourcePath.endsWith(".pasm"):
+        echo fmt"File is not .pasm: `{sourcePath}`"
+        quit 1
+
+    let source: string = readFile(sourcePath)
     if source.len == 0:
-        echo "File is empty: ", argv[0]
-        quit(1)
+        echo fmt"File is empty: `{sourcePath}`"
+        quit 1
 
     var m = Machine()
-    m.programName = argv[0]
+    m.programName = sourcePath
 
     m.compile(source)
     m.run()
