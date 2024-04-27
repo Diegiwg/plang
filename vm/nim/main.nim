c3f6354b9bddad8982d860ce69f5b1c5a3fa102d
Diegiwg <diegiwg@gmail.com>
2024-04-26 23:55:11 -0300

    feat: implement `cast` and `dup`

--- vm/nim/main.nim
+++ vm/nim/main.nim
@@ -13,11 +13,14 @@ type
         of VK_Int: i: int
 
     InstructionKind = enum
-        Pop, Push, Add, Print, Label
+        Unknown, Cast, Dup, Pop, Push, Add, Print, Label
 
     Instruction = object
         loc: int
         case kind: InstructionKind
+        of Unknown: discard
+        of Cast: type_target: ValueKind
+        of Dup: value_target: int
         of Pop: discard
         of Push: operand: Value
         of Add: discard
@@ -32,6 +35,30 @@ type
 
     LogLevel = enum
         Log_Info, Log_Warning, Log_Error
+
+proc strToValueKind(s: string): ValueKind =
+    case s:
+    of "void": return VK_Void
+    of "string": return VK_String
+    of "int": return VK_Int
+    else: return VK_Void
+
+proc strToInstructionKind(s: string): InstructionKind =
+    static:
+        if ord(high(InstructionKind)) + 1 != 8:
+            echo "ERROR: InstructionKind out of range"
+            quit 1
+
+    case s:
+    of "unknown": return Unknown
+    of "cast": return Cast
+    of "dup": return Dup
+    of "pop": return Pop
+    of "push": return Push
+    of "add": return Add
+    of "print": return Print
+    of "label": return Label
+    else: return Unknown
 
 proc log(m: Machine, loc: int, level: LogLevel, msg: string) =
     let logLevel = case level:
@@ -53,17 +80,35 @@ proc compile(m: Machine, source: string) =
         let parts = line.splitWhitespace()
         if parts.len == 0: continue
 
-        let cmd = parts[0]
+        let cmd = strToInstructionKind(parts[0])
         case cmd:
-        
-        of "label":
+        of Cast:
+            if parts.len < 2 or parts[1].len == 0: m.log(lineCount, Log_Error, "Missing operand for `cast` instruction")
+            if parts[1].len < 0: m.log(lineCount, Log_Error, "Invalid operand for `cast` instruction")
+            
+            var target: ValueKind = strToValueKind(parts[1])
+            if target == VK_Void: m.log(lineCount, Log_Error, "Invalid operand for `cast` instruction")
+
+            m.program.add(Instruction(loc: lineCount, kind: Cast, type_target: target))
+
+        of Dup:
+            if parts.len < 2 or parts[1].len == 0: m.log(lineCount, Log_Error, "Missing operand for `dup` instruction")
+            if parts[1].len < 0: m.log(lineCount, Log_Error, "Invalid operand for `dup` instruction") 
+            
+            var target: int
+            try: target = parseInt(parts[1])
+            except: m.log(lineCount, Log_Error, "Invalid operand for `dup` instruction")
+                
+            m.program.add(Instruction(loc: lineCount, kind: Dup, value_target: target))
+
+        of Label:
             m.program.add(Instruction(loc: lineCount, kind: Label, ip: instructionCount))
 
-        of "pop":
+        of Pop:
             m.program.add(Instruction(loc: lineCount, kind: Pop))
 
-        of "push":
-            if parts.len < 2 or parts[1].len == 0: m.log(lineCount, Log_Warning, "Missing operand for `push` instruction")
+        of Push:
+            if parts.len < 2 or parts[1].len == 0: m.log(lineCount, Log_Error, "Missing operand for `push` instruction")
 
             var value: Value = Value(kind: VK_Void)
 
@@ -76,19 +121,18 @@ proc compile(m: Machine, source: string) =
                 if rest[0] == '"' and rest[^1] == '"':
                     value = Value(kind: VK_String, s: rest[1..^2])
 
-            if value.kind == VK_Void: m.log(lineCount, Log_Warning, fmt("Unknown operand `{rest}` for `push` instruction. `{VK_Void}` assumed"))
+            if value.kind == VK_Void: m.log(lineCount, Log_Error, fmt("Unknown operand `{rest}` for `push` instruction. `{VK_Void}` assumed"))
 
             m.program.add(Instruction(loc: lineCount, kind: Push, operand: value))
 
-        of "add":
+        of Add:
             m.program.add(Instruction(loc: lineCount, kind: Add))
 
-        of "print":
+        of Print:
             m.program.add(Instruction(loc: lineCount, kind: Print))
 
-        else:
-            m.log(lineCount, Log_Warning, fmt("Unknown instruction `{cmd}` skipped"))
-            continue
+        of Unknown:
+            m.log(lineCount, Log_Error, fmt("Unknown instruction `{cmd}` skipped"))
 
         inc instructionCount
 
@@ -97,6 +141,30 @@ proc run(m: Machine) =
         let instruction = m.program[m.ip]
 
         case instruction.kind:
+        of Cast:
+            if m.stack.len == 0: m.log(instruction.loc, Log_Error, "No value on stack to cast")
+
+            let value = m.stack.pop()
+            case value.kind:
+            of VK_Void: m.log(instruction.loc, Log_Error, "Casting `{VK_Void}` is not allowed")
+            of VK_Int:
+                case instruction.type_target:
+                of VK_Void: m.log(instruction.loc, Log_Error, "Casting `{VK_Void}` is not allowed")
+                of VK_Int: m.log(instruction.loc, Log_Warning, "Casting `{VK_Int}` to `{VK_Int}` does nothing")
+                of VK_String: m.stack.add(Value(kind: VK_String, s: $value.i))
+            of VK_String:
+                case instruction.type_target:
+                of VK_Void: m.log(instruction.loc, Log_Error, "Casting `{VK_Void}` is not allowed")
+                of VK_String: m.log(instruction.loc, Log_Warning, "Casting `{VK_String}` to `{VK_String}` does nothing")
+                of VK_Int:
+                    try: m.stack.add(Value(kind: VK_Int, i: parseInt(value.s)))
+                    except: m.log(instruction.loc, Log_Error, "Casting `{VK_String}: {value.s}` to `{VK_Int}` failed")
+
+        of Dup:
+            let target = m.stack.len - instruction.value_target - 1
+            if target < 0 or target >= m.stack.len: m.log(instruction.loc, Log_Error, "Trying to duplicate value out of stack")
+            m.stack.add(m.stack[target])
+
         of Label: discard
 
         of Print:
@@ -126,6 +194,9 @@ proc run(m: Machine) =
             of VK_Int: m.stack.add(Value(kind: VK_Int, i: a.i + b.i))
             of VK_String: m.stack.add(Value(kind: VK_String, s: a.s & b.s))
             of VK_Void: discard
+        
+        of Unknown:
+            m.log(instruction.loc, Log_Error, fmt("Unknown instruction"))
 
         inc m.ip
 
